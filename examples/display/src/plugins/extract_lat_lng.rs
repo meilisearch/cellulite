697e2c84f15f84aabc1c5531f73b50033dc1acc9
Tamo <tamo@meilisearch.com>
2025-04-05 17:10:36 +0200

    display the cell associated with where you clicked
    Former-commit-id: 4f52d9865ef961e7f3863e37c8f577a7a7bffa05

--- examples/display/src/plugins/extract_lat_lng.rs
+++ examples/display/src/plugins/extract_lat_lng.rs
@@ -1,13 +1,14 @@
 use std::sync::{atomic::Ordering, Arc};
 
-use egui::Vec2;
+use egui::{Ui, Vec2};
+use h3o::{LatLng, Resolution};
 use walkers::Plugin;
 
 use crate::utils::AtomicF64;
 
 /// Plugin used to extract the position of the mouse. It's then displayed to the side panel.
 #[derive(Clone)]
-pub struct ExtractLatLng {
+pub struct ExtractMousePos {
     pub current_lat: Arc<AtomicF64>,
     pub current_lng: Arc<AtomicF64>,
 
@@ -15,9 +16,9 @@ pub struct ExtractLatLng {
     pub clicked_lng: Arc<AtomicF64>,
 }
 
-impl Default for ExtractLatLng {
+impl Default for ExtractMousePos {
     fn default() -> Self {
-        ExtractLatLng {
+        ExtractMousePos {
             current_lat: Arc::new(AtomicF64::new(0.0)),
             current_lng: Arc::new(AtomicF64::new(0.0)),
             clicked_lat: Arc::new(AtomicF64::new(0.0)),
@@ -26,7 +27,30 @@ impl Default for ExtractLatLng {
     }
 }
 
-impl Plugin for ExtractLatLng {
+impl ExtractMousePos {
+    pub fn ui(&self, ui: &mut Ui) {
+        ui.label(format!(
+            "mouse: {:.5},{:.5}",
+            self.current_lat.load(Ordering::Relaxed),
+            self.current_lng.load(Ordering::Relaxed)
+        ));
+        let lat = self.clicked_lat.load(Ordering::Relaxed);
+        let lng = self.clicked_lng.load(Ordering::Relaxed);
+        ui.label(format!("Last click: {lat:.5},{lng:.5}",));
+
+        let lat_lng = LatLng::new(lat, lng).unwrap();
+        for res in Resolution::range(Resolution::Zero, Resolution::Fifteen) {
+            let cell = lat_lng.to_cell(res);
+            let cell = u64::from(cell);
+            ui.hyperlink_to(
+                format!("Resolution {res}: {cell:x}"),
+                format!("https://h3geo.org/#hex={cell:x}"),
+            );
+        }
+    }
+}
+
+impl Plugin for ExtractMousePos {
     fn run(
         self: Box<Self>,
         _ui: &mut egui::Ui,
