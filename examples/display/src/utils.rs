48fd9de2387c0a849f0bd67bc846780c95e2f2e2
Tamo <tamo@meilisearch.com>
2025-04-10 22:53:29 +0200

    add an option to display either the h3 cell or the bounding boxes

--- examples/display/src/utils.rs
+++ examples/display/src/utils.rs
@@ -32,10 +32,19 @@ pub fn project_line_string(projector: &Projector, line: &[Coord]) -> Vec<Pos2> {
         .collect()
 }
 
-pub fn display_cell(projector: &Projector, painter: &Painter, cell: CellIndex, color: Color32) {
-    // let solvent = h3o::geom::SolventBuilder::new().build();
-    // let cell_polygon = solvent.dissolve(Some(cell)).unwrap();
-    let cell_polygon = bounding_box(cell);
+pub fn display_cell(
+    projector: &Projector,
+    painter: &Painter,
+    cell: CellIndex,
+    color: Color32,
+    as_bounding_box: bool,
+) {
+    let cell_polygon = if as_bounding_box {
+        bounding_box(cell)
+    } else {
+        let solvent = h3o::geom::SolventBuilder::new().build();
+        solvent.dissolve(Some(cell)).unwrap()
+    };
     let cell_polygon = &cell_polygon.0[0];
     let line = project_line_string(projector, &cell_polygon.exterior().0);
     painter.line(
