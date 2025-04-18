21174fe34abbfb60938ba1f256fdd9b719e93400
Luna Ferraraccio <luna.ferraraccio@RZ-360Learning-FVFHJ37DQ05G.local>
2025-04-18 11:31:05 +0200

    prepared for shape insertion
    Former-commit-id: 0b06ccc972f84ee17486fd99ed91a365e40303f5

--- examples/display/src/plugins/insert_into_database.rs
+++ examples/display/src/plugins/insert_into_database.rs
@@ -3,24 +3,34 @@ use std::sync::{
     Arc,
 };
 
-use egui::Vec2;
+use egui::{mutex::Mutex, RichText, Ui, Vec2};
 use h3o::LatLng;
 use walkers::Plugin;
 
 use crate::runner::Runner;
 
+#[derive(Clone, Copy, PartialEq, Default, Debug)]
+pub enum InsertMode {
+    #[default]
+    Disable,
+    Point,
+    MultiPoint,
+}
+
 /// Plugin used to insert position when a right click happens.
 #[derive(Clone)]
 pub struct InsertIntoDatabase {
-    pub disabled: Arc<AtomicBool>,
+    pub insert_mode: Arc<Mutex<InsertMode>>,
     runner: Runner,
+    pub filtering: Arc<AtomicBool>,
 }
 
 impl InsertIntoDatabase {
     pub fn new(runner: Runner) -> Self {
         InsertIntoDatabase {
-            disabled: Arc::default(),
+            insert_mode: Arc::default(),
             runner,
+            filtering: Arc::default(),
         }
     }
 
@@ -30,6 +40,23 @@ impl InsertIntoDatabase {
             let lng = rand::random_range(-180.0..=180.0);
             self.runner.add_point(LatLng::new(lat, lng).unwrap());
         }
+    }
+
+    pub fn ui(&self, ui: &mut Ui) {
+        ui.collapsing(RichText::new("Insert").heading(), |ui| {
+            let mut insert_mode = self.insert_mode.lock();
+
+            egui::ComboBox::from_label("Geometry to insert")
+                .selected_text(format!("{insert_mode:?}"))
+                .show_ui(ui, |ui| {
+                    ui.selectable_value(&mut *insert_mode, InsertMode::Disable, "Disable");
+                    ui.selectable_value(&mut *insert_mode, InsertMode::Point, "Point");
+                    ui.selectable_value(&mut *insert_mode, InsertMode::MultiPoint, "Multipoint");
+                });
+            if self.filtering.load(Ordering::Relaxed) {
+                *insert_mode = InsertMode::Disable;
+            }
+        });
     }
 }
 
@@ -43,10 +70,17 @@ impl Plugin for InsertIntoDatabase {
         let Some(pos) = response.hover_pos() else {
             return;
         };
-        if !self.disabled.load(Ordering::Relaxed) && response.secondary_clicked() {
-            let pos = projector.unproject(Vec2::new(pos.x, pos.y));
-            self.runner
-                .add_point(LatLng::new(pos.y(), pos.x()).unwrap());
+
+        if response.secondary_clicked() {
+            match *self.insert_mode.lock() {
+                InsertMode::Disable => (),
+                InsertMode::Point => {
+                    let pos = projector.unproject(Vec2::new(pos.x, pos.y));
+                    self.runner
+                        .add_point(LatLng::new(pos.y(), pos.x()).unwrap());
+                }
+                InsertMode::MultiPoint => todo!(),
+            }
         }
     }
 }
