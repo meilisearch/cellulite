5530fd821332d6940664745a29bd172ae6c81e0f
Tamo <tamo@meilisearch.com>
2025-04-22 23:23:25 +0200

    add support of polygon on the ui
    Former-commit-id: 62b9722a5a34b880f51e3211349ec254327e71d7

--- examples/display/src/plugins/insert_into_database.rs
+++ examples/display/src/plugins/insert_into_database.rs
@@ -4,6 +4,7 @@ use std::sync::{
     Arc,
 };
 
+use egui::Pos2;
 use egui::{mutex::Mutex, RichText, Ui, Vec2};
 use geo_types::{Coord, Point};
 use walkers::Plugin;
@@ -16,6 +17,7 @@ pub enum InsertMode {
     Disable,
     Point,
     MultiPoint,
+    Polygon,
 }
 
 /// Plugin used to insert position when a right click happens.
@@ -55,6 +57,7 @@ impl InsertIntoDatabase {
                     ui.selectable_value(&mut *insert_mode, InsertMode::Disable, "Disable");
                     ui.selectable_value(&mut *insert_mode, InsertMode::Point, "Point");
                     ui.selectable_value(&mut *insert_mode, InsertMode::MultiPoint, "Multipoint");
+                    ui.selectable_value(&mut *insert_mode, InsertMode::Polygon, "Polygon");
                 });
 
             match *insert_mode {
@@ -78,6 +81,28 @@ impl InsertIntoDatabase {
                         }
                     }
                 }
+                InsertMode::Polygon => {
+                    let mut points = self.insert_shape.lock();
+                    ui.label(format!("Points collected: {}", points.len()));
+                    if points.len() >= 3 {
+                        if ui.button("Complete polygon").clicked() {
+                            let mut polygon_points = points
+                                .iter()
+                                .map(|coord| vec![coord.x, coord.y])
+                                .collect::<Vec<_>>();
+                            // Close the polygon by adding the first point at the end
+                            polygon_points.push(polygon_points[0].clone());
+                            self.runner
+                                .add_shape(geojson::Value::Polygon(vec![polygon_points]));
+                            points.clear();
+                        }
+                    }
+                    if !points.is_empty() {
+                        if ui.button("Clear points").clicked() {
+                            points.clear();
+                        }
+                    }
+                }
             }
 
             if self.filtering.load(Ordering::Relaxed) {
@@ -94,27 +119,70 @@ impl Plugin for InsertIntoDatabase {
         response: &egui::Response,
         projector: &walkers::Projector,
     ) {
-        // Draw pending points in yellow
-        if *self.insert_mode.lock() == InsertMode::MultiPoint {
-            let points = self.insert_shape.lock();
-            for point in points.iter() {
-                let center = projector.project(Point::new(point.x, point.y));
-                let size = 8.0;
-                ui.painter().line(
-                    vec![
-                        (center - Vec2::splat(size)).to_pos2(),
-                        (center + Vec2::splat(size)).to_pos2(),
-                    ],
-                    egui::Stroke::new(4.0, egui::Color32::YELLOW),
-                );
-                ui.painter().line(
-                    vec![
-                        (center + Vec2::new(size, -size)).to_pos2(),
-                        (center + Vec2::new(-size, size)).to_pos2(),
-                    ],
-                    egui::Stroke::new(4.0, egui::Color32::YELLOW),
-                );
+        match *self.insert_mode.lock() {
+            InsertMode::MultiPoint => {
+                let points = self.insert_shape.lock();
+                for point in points.iter() {
+                    let center = projector.project(Point::new(point.x, point.y));
+                    let size = 8.0;
+                    ui.painter().line(
+                        vec![
+                            (center - Vec2::splat(size)).to_pos2(),
+                            (center + Vec2::splat(size)).to_pos2(),
+                        ],
+                        egui::Stroke::new(4.0, egui::Color32::YELLOW),
+                    );
+                    ui.painter().line(
+                        vec![
+                            (center + Vec2::new(size, -size)).to_pos2(),
+                            (center + Vec2::new(-size, size)).to_pos2(),
+                        ],
+                        egui::Stroke::new(4.0, egui::Color32::YELLOW),
+                    );
+                }
             }
+            InsertMode::Polygon => {
+                let points = self.insert_shape.lock();
+                if points.len() >= 1 {
+                    let mut line: Vec<Pos2> = points
+                        .iter()
+                        .map(|point| projector.project(Point::new(point.x, point.y)).to_pos2())
+                        .collect();
+
+                    // Add mouse position or close polygon
+                    if let Some(mouse_pos) = response.hover_pos() {
+                        line.push(mouse_pos);
+                    } else {
+                        line.push(line[0]); // Close polygon by adding first point
+                    }
+
+                    ui.painter().add(egui::Shape::line(
+                        line,
+                        egui::Stroke::new(8.0, egui::Color32::YELLOW),
+                    ));
+                }
+
+                // Draw yellow crosses at each point
+                for point in points.iter() {
+                    let center = projector.project(Point::new(point.x, point.y));
+                    let size = 8.0;
+                    ui.painter().line(
+                        vec![
+                            (center - Vec2::splat(size)).to_pos2(),
+                            (center + Vec2::splat(size)).to_pos2(),
+                        ],
+                        egui::Stroke::new(4.0, egui::Color32::YELLOW),
+                    );
+                    ui.painter().line(
+                        vec![
+                            (center + Vec2::new(size, -size)).to_pos2(),
+                            (center + Vec2::new(-size, size)).to_pos2(),
+                        ],
+                        egui::Stroke::new(4.0, egui::Color32::YELLOW),
+                    );
+                }
+            }
+            _ => {}
         }
         let Some(pos) = response.hover_pos() else {
             return;
@@ -129,6 +197,13 @@ impl Plugin for InsertIntoDatabase {
                         .add_shape(geojson::Value::Point(vec![pos.x(), pos.y()]));
                 }
                 InsertMode::MultiPoint => {
+                    let pos = projector.unproject(Vec2::new(pos.x, pos.y));
+                    self.insert_shape.lock().push(Coord {
+                        x: pos.x(),
+                        y: pos.y(),
+                    });
+                }
+                InsertMode::Polygon => {
                     let pos = projector.unproject(Vec2::new(pos.x, pos.y));
                     self.insert_shape.lock().push(Coord {
                         x: pos.x(),
