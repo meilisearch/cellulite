73c542fd15ba83a106d8280c07631c2c88fd77c4
Tamo <tamo@meilisearch.com>
2025-04-25 14:33:51 +0200

    get rids of the bounding box options since it’s not used anymore
    Former-commit-id: 837d3cdca609f01cf154c431b06a058d43d2c354

--- examples/display/src/utils.rs
+++ examples/display/src/utils.rs
@@ -1,6 +1,5 @@
 use std::sync::atomic::{AtomicU64, Ordering};
 
-use cellulite::geom::bounding_box;
 use egui::{epaint::PathStroke, Color32, Painter, Pos2};
 use geo_types::Coord;
 use h3o::CellIndex;
@@ -37,14 +36,9 @@ pub fn display_cell(
     painter: &Painter,
     cell: CellIndex,
     color: Color32,
-    as_bounding_box: bool,
 ) {
-    let cell_polygon = if as_bounding_box {
-        bounding_box(cell)
-    } else {
-        let solvent = h3o::geom::SolventBuilder::new().build();
-        solvent.dissolve(Some(cell)).unwrap()
-    };
+    let solvent = h3o::geom::SolventBuilder::new().build();
+    let cell_polygon = solvent.dissolve(Some(cell)).unwrap();
     let cell_polygon = &cell_polygon.0[0];
     let line = project_line_string(projector, &cell_polygon.exterior().0);
     painter.line(
