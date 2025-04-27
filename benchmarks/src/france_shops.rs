88cdb6473e1486e9815c357361b55bcbf2f00b4f
Tamo <tamo@meilisearch.com>
2025-04-28 00:34:34 +0200

    store the names of the documents in the benchmarks
    Former-commit-id: 23763337e30f598751320b41345ec01039e303af

--- benchmarks/src/france_shops.rs
+++ benchmarks/src/france_shops.rs
@@ -4,31 +4,31 @@ use std::io::BufReader;
 
 use flate2::bufread::GzDecoder;
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
 struct Schema {
     features: Vec<Feature>,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
 struct Feature {
     geometry: Geometry,
     #[allow(unused)]
     properties: Properties,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
 struct Geometry {
     coordinates: [f64; 2],
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
 #[allow(unused)]
 struct Properties {
     shop: Option<String>,
     name: Option<String>,
 }
 
-pub fn parse() -> impl Iterator<Item = GeoJson> {
+pub fn parse() -> impl Iterator<Item = (String, GeoJson)> {
     let file = std::fs::File::open("assets/france-shops.json.gz").unwrap();
     let file = BufReader::new(file);
     let file = GzDecoder::new(file);
@@ -38,7 +38,7 @@ pub fn parse() -> impl Iterator<Item = GeoJson> {
     input
         .features
         .into_iter()
-        .map(|feature| feature.geometry.coordinates)
-        .map(move |[x, y]| projection.convert((x, y)).unwrap())
-        .map(|(lat, lng)| GeoJson::Geometry(geojson::Geometry::new(Value::Point(vec![lat, lng]))))
+        .map(|feature| (feature.properties.name.unwrap_or_else(|| feature.properties.shop.unwrap_or_default()), feature.geometry.coordinates))
+        .map(move |(shop, [x, y])| (shop, projection.convert((x, y)).unwrap()))
+        .map(|(shop, (lat, lng))| (shop, GeoJson::Geometry(geojson::Geometry::new(Value::Point(vec![lat, lng])))))
 }
