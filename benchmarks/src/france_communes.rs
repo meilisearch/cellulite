197494a033ae658a8d65fcc56496b590b0d02546
Tamo <tamo@meilisearch.com>
2025-06-08 15:53:49 +0200

    add more benchmarks with parts of fronce
    Former-commit-id: 51e0e33fb505b7a4dcf413387ca8cc2a68cf7a17

---
+++ benchmarks/src/france_communes.rs
@@ -0,0 +1,21 @@
+// Importer for https://github.com/gregoiredavid/france-geojson
+
+use std::{fs, io::BufReader};
+
+use geojson::{GeoJson, de::deserialize_feature_collection_to_vec};
+
+pub fn parse() -> impl Iterator<Item = (String, GeoJson)> {
+    let file = fs::File::open("assets/france-geojson/communes.geojson").unwrap();
+    let file = BufReader::new(file);
+    let input: Vec<serde_json::Value> = deserialize_feature_collection_to_vec(file).unwrap();
+        input.into_iter().map(|value| {
+            (
+                format!(
+                    "{}, {} - commune",
+                    value["nom"].as_str().unwrap().to_lowercase(),
+                    value["code"].as_str().unwrap()
+                ),
+            GeoJson::from_json_value(value["geometry"].clone()).unwrap(),
+        )
+    })
+}
