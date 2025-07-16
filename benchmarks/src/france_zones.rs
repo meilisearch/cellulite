08f6df459d790afef7c9cf883336882275e6e6dd
Tamo <tamo@meilisearch.com>
2025-07-16 18:48:49 +0200

    stop being a coward and use the full french zones :grin:
    Former-commit-id: a944b1d8c9e221e2c1578fc08b1ad519975763c6

--- benchmarks/src/france_zones.rs
+++ benchmarks/src/france_zones.rs
@@ -5,5 +5,5 @@ pub fn parse() -> impl Iterator<Item = (String, GeoJson)> {
         .chain(crate::france_cantons::parse())
         .chain(crate::france_communes::parse())
         .chain(crate::france_departements::parse())
-    //    .chain(crate::france_regions::parse())
+        .chain(crate::france_regions::parse())
 }
