edf37f76ea9569b71cc1f21c03b473f9a38542cf
Tamo <tamo@meilisearch.com>
2025-07-18 13:51:42 +0200

    move the errors definition to their own file
    Former-commit-id: a538c4563819e15411d07773bbde75b159dd7793

---
+++ src/error.rs
@@ -0,0 +1,35 @@
+use h3o::error::{InvalidLatLng, InvalidGeometry};
+
+use crate::ItemId;
+
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    // User errors
+    #[error("Document with id `{0}` contains a {1} but only `Geometry` type is supported")]
+    InvalidGeoJsonTypeFormat(ItemId, &'static str),
+    #[error("Document with id `{0}` contains a {1} but only `Point`, `Polygon`, `MultiPoint` and `MultiPolygon` types are supported")]
+    InvalidGeometryTypeFormat(ItemId, &'static str),
+
+    // External errors, sometimes it's a user error and sometimes it's not
+    #[error(transparent)]
+    Heed(#[from] heed::Error),
+    #[error(transparent)]
+    InvalidLatLng(#[from] InvalidLatLng),
+    #[error(transparent)]
+    InvalidGeometry(#[from] InvalidGeometry),
+    #[error(transparent)]
+    InvalidGeoJson(#[from] geojson::Error),
+
+    // Internal errors
+    #[error("Internal error: unexpected document id `{0}` missing at `{1}`")]
+    InternalDocIdMissing(ItemId, String),
+}
+
+#[macro_export]
+macro_rules! pos {
+    () => {
+        format!("{}:{}:{}", file!(), line!(), column!())
+    };
+}
+
