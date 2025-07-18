f48471b28c07903bcd7f4a65b51efebcaa614506
Tamo <tamo@meilisearch.com>
2025-07-18 17:30:27 +0200

    provide options to skip inserting the documents, commiting or building the index
    Former-commit-id: 87f50abbe6d768a886ab2c80fd18fc39539d35c2

--- benchmarks/src/france_cadastre_parcelles.rs
+++ benchmarks/src/france_cadastre_parcelles.rs
@@ -6,25 +6,28 @@ use flate2::bufread::GzDecoder;
 use geojson::{GeoJson, de::deserialize_feature_collection_to_vec};
 
 pub fn parse(selector: Vec<String>) -> impl Iterator<Item = (String, GeoJson)> {
-    fs::read_dir("assets/cadastre_parcelle").unwrap()
-    .map(|entry| entry.unwrap())
-    .filter(move |dir| {
-        selector.is_empty() || selector.iter().any(|s| dir.path().to_str().unwrap().contains(s))
-    })
-    .flat_map(|dir| {
-        println!("Importing {}", dir.path().display());
-
-        let file = std::fs::File::open(dir.path()).unwrap();
-        let file = BufReader::new(file);
-        let file = GzDecoder::new(file);
-        let input: Vec<serde_json::Value> = deserialize_feature_collection_to_vec(file).unwrap();
-        input.into_iter().map(|value| {
-            (
-                format!(
-                    "{}", value["properties"]["id"]
-                ),
-                GeoJson::from_json_value(value["geometry"].clone()).unwrap(),
-            )
+    fs::read_dir("assets/cadastre_parcelle")
+        .unwrap()
+        .map(|entry| entry.unwrap())
+        .filter(move |dir| {
+            selector.is_empty()
+                || selector
+                    .iter()
+                    .any(|s| dir.path().to_str().unwrap().contains(s))
         })
-    })
+        .flat_map(|dir| {
+            println!("Importing {}", dir.path().display());
+
+            let file = std::fs::File::open(dir.path()).unwrap();
+            let file = BufReader::new(file);
+            let file = GzDecoder::new(file);
+            let input: Vec<serde_json::Value> =
+                deserialize_feature_collection_to_vec(file).unwrap();
+            input.into_iter().map(|value| {
+                (
+                    format!("{}", value["properties"]["id"]),
+                    GeoJson::from_json_value(value["geometry"].clone()).unwrap(),
+                )
+            })
+        })
 }
