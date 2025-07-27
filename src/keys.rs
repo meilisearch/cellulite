6c87071e8be064033a3dd0f0107466f3fd1eb4f5
Tamo <tamo@meilisearch.com>
2025-07-27 17:10:03 +0200

    use different DB to store items and cells

--- src/keys.rs
+++ src/keys.rs
@@ -1,25 +1,39 @@
+use std::borrow::Cow;
+
 use h3o::CellIndex;
-use heed::byteorder::{BigEndian, ByteOrder};
+use heed::{
+    byteorder::{BE, BigEndian, ByteOrder},
+    types::U64,
+};
 
-use crate::ItemId;
+pub struct ItemKeyCodec;
 
-pub struct KeyCodec;
+impl heed::BytesEncode<'_> for ItemKeyCodec {
+    type EItem = u32;
 
-impl<'a> heed::BytesEncode<'a> for KeyCodec {
+    fn bytes_encode(item: &'_ Self::EItem) -> Result<std::borrow::Cow<'_, [u8]>, heed::BoxedError> {
+        let aligned_item = *item as u64;
+        Ok(Cow::from(aligned_item.to_be_bytes().to_vec()))
+    }
+}
+
+impl heed::BytesDecode<'_> for ItemKeyCodec {
+    type DItem = u32;
+
+    fn bytes_decode(bytes: &'_ [u8]) -> Result<Self::DItem, heed::BoxedError> {
+        U64::<BE>::bytes_decode(bytes).map(|n| n as u32)
+    }
+}
+
+pub struct CellKeyCodec;
+
+impl<'a> heed::BytesEncode<'a> for CellKeyCodec {
     type EItem = Key;
 
     fn bytes_encode(key: &'a Self::EItem) -> Result<std::borrow::Cow<'a, [u8]>, heed::BoxedError> {
         const ALIGNMENT: usize = std::mem::size_of::<f64>();
         let mut ret: Vec<u8>;
         match key {
-            Key::Item(item) => {
-                let capacity = size_of::<KeyVariant>() + size_of_val(item);
-                let missing_to_align = ALIGNMENT - (capacity % ALIGNMENT);
-                ret = Vec::with_capacity(capacity + missing_to_align);
-                ret.push(KeyVariant::Item as u8);
-                ret.extend_from_slice(&item.to_be_bytes());
-                ret.extend(std::iter::repeat_n(0, missing_to_align));
-            }
             Key::Cell(cell) => {
                 let capacity = size_of::<KeyVariant>() + size_of_val(cell);
                 let missing_to_align = ALIGNMENT - (capacity % ALIGNMENT);
@@ -43,17 +57,13 @@ impl<'a> heed::BytesEncode<'a> for KeyCodec {
     }
 }
 
-impl heed::BytesDecode<'_> for KeyCodec {
+impl heed::BytesDecode<'_> for CellKeyCodec {
     type DItem = Key;
 
     fn bytes_decode(bytes: &'_ [u8]) -> Result<Self::DItem, heed::BoxedError> {
         let variant = bytes[0];
         let bytes = &bytes[std::mem::size_of_val(&variant)..];
         let key = match variant {
-            v if v == KeyVariant::Item as u8 => {
-                let item = BigEndian::read_u32(bytes);
-                Key::Item(item)
-            }
             v if v == KeyVariant::Cell as u8 => {
                 let cell = BigEndian::read_u64(bytes);
                 Key::Cell(cell.try_into()?)
@@ -71,7 +81,6 @@ impl heed::BytesDecode<'_> for KeyCodec {
 }
 
 pub enum Key {
-    Item(ItemId),
     Cell(CellIndex),
     InnerShape(CellIndex),
 }
@@ -79,7 +88,6 @@ pub enum Key {
 #[repr(u8)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum KeyVariant {
-    Item = 0,
     Cell = 1,
     InnerShape = 2,
 }
